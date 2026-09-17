bb06321e69b53d5cb740ef255cb083a5b6494775
Chaitanyaagra <chaitanyaagrawal77@gmail.com>
2026-09-17 15:28:03 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,14 @@
 /*
-  OPTIONAL SHARED FIREBASE CONFIG
-  Paste the Firebase Web App config object below once before deploying this folder.
-  It is safe for Firebase web config to be public; security is enforced by Authentication + Firestore Rules.
-
-  Example:
-  window.PM_FIREBASE_CONFIG = {
-    apiKey: "...",
-    authDomain: "your-project.firebaseapp.com",
-    projectId: "your-project",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "...",
-    appId: "..."
-  };
+  SHARED FIREBASE CONFIG
+  This is safe to be public; security is enforced by Authentication + Firestore Rules,
+  not by hiding this file. Baking it in here means the app always has a working config,
+  even if a device has trouble saving the "Connect" step's local copy.
 */
-window.PM_FIREBASE_CONFIG = window.PM_FIREBASE_CONFIG || null;
+window.PM_FIREBASE_CONFIG = window.PM_FIREBASE_CONFIG || {
+  apiKey: "AIzaSyAfu0-Ih3nFtimeL63_DTPcBZ6MdJxp22I",
+  authDomain: "list-1a1d0.firebaseapp.com",
+  projectId: "list-1a1d0",
+  storageBucket: "list-1a1d0.firebasestorage.app",
+  messagingSenderId: "136541430311",
+  appId: "1:136541430311:web:d9856b2da1b1e9d246a402"
+};
